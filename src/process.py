eb1f00e52dac276b57b9bfd89c44fc6fd2b8e5e4
Nate Novak <nate.j.nov@gmail.com>
2022-10-06 12:52:16 -0400

    Added calculation of img chromaticity

--- src/process.py
+++ src/process.py
@@ -18,11 +18,11 @@ def test():
     cv2.destroyAllWindows()
 
 def readimgs(dir): 
-    '''Function to read all the images in a directory
-        @param dir: indicates the directory to walk
-        @return: dict of numpy arrays {image name : numpyarray}
     '''
-    
+    Function to read all the images in a directory
+    @param dir: indicates the directory to walk
+    @return: dict of numpy arrays {image name : np array}
+    '''
     images = {}
     for file in os.listdir(dir): 
         name = os.path.join(dir, file)
@@ -32,18 +32,42 @@ def readimgs(dir):
             ext = split[1]
             if ext == ".jpg" or ext == ".png": 
                 # if we get here, this is 1.) a file, and 2.) the file is an image
-                print("here")
                 img = cv2.imread(name, 1)
                 images[imgName] = img
     
     return images
 
 
+def calcImgChromaticity(imagesDict):
+    '''
+    Function to calculate the chromaticity of each pixel in the image
+    @param imagesDict: dictionary of images {image name : np array}
+    @return: dictionary of image names and their chromaticity {image name : np array of (G/R, B/R) chromaticity }
+    '''
+    chromaDict = {}
+    for key in imagesDict: 
+        bgrImage = imagesDict[key]
+        height, width, depth = bgrImage.shape
+        chroma = np.zeros((height, width, 2), dtype=float)
+        for row in range(height): 
+            for col in range(width): 
+                pixel = bgrImage[row, col]
+                b, g, r = pixel[0], pixel[1], pixel[2]
+
+                if(r == 0): continue
+
+                chroma[row, col, 0] = g / r
+                chroma[row, col, 1] = b / r
+        
+        chromaDict[key] = chroma
+
+    return chromaDict
+
 
 def main(): 
     images = readimgs("./imgs/")
-
-
+    chromas = calcImgChromaticity(images)
+    print(chromas)
 
 if __name__ == "__main__":
     main()
