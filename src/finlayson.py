ae46f59e52df68d96326c548e9ed2bac716535fa
Nate Novak <nate.j.nov@gmail.com>
2022-10-06 12:20:39 -0400

    Added ability to read files

--- src/finlayson.py
+++ src/finlayson.py
@@ -2,18 +2,10 @@
 # CS 7180: Advanced perception 
 # Program to conduct Finlayson Intrinisic imaging algorithm
 
-import cv2 as cv2
-
-def test():
-    print("hello world")
-    img = cv2.imread("./images/pic.0001.jpg", 1)
-    print(img)
-    cv2.imshow("test", img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+import process as p
 
 def main(): 
-    test()
+    p.test()
 
 if __name__ == '__main__':
     main()
---
+++ src/process.py
@@ -0,0 +1,46 @@
+# Brenden Collins // Nate Novak
+# CS 7180: Advanced perception 
+# Module to hold all of the steps of the algorithm
+
+from genericpath import isfile
+import cv2
+from importlib_metadata import files
+import numpy as np
+import os
+
+def test():
+    '''Function to test dependencies'''
+    print("hello world")
+    img = cv2.imread("./imgs/pic.0001.jpg", 1)
+    print(img)
+    cv2.imshow("test", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+def readimg(dir): 
+    '''Function to read all the images in a directory
+        @param dir: indicates the directory to walk
+        @return: dict of numpy arrays {image name : numpyarray}
+    '''
+    
+    images = {}
+    for file in os.listdir(dir): 
+        name = os.join(dir, file)
+        if os.isfile(name): 
+            split = os.path.splitext(name)
+            imgName = split[0]
+            ext = split[1]
+            if ext == ".jpg" or ext == ".png": 
+                # if we get here, this is 1.) a file, and 2.) the file is an image
+                img = cv2.imread(name, 1)
+                images[imgName] = img
+    
+    return images
+
+
+
+def main(): 
+    test()
+
+if __name__ == "__main__":
+    main()
